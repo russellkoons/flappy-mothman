d005d3e1c4f3b80c2887a67ba368a6c5a5fd8bad
russellkoons <rkoons@gmail.com>
2020-08-15 11:12:14 -0400

    a lot of new functions

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 var mainState = {
   preload: function() {
-
+    // Load mothman
+    game.load.image('mothman', 'assets/bird.png');
   },
 
   create: function() {
+    // Change background color
+    game.state.backgroundColor = '#71c5cf';
 
+    // Set physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Display mothman
+    this.mothman = game.add.sprite(100, 245, 'mothman');
+
+    // Add physics to mothman
+    game.physics.arcade.enable(this.mothman);
+
+    // Add gravity to mothman
+    this.mothman.body.gravity.y = 1000;
+
+    // jump on space
+    var spaceKey = game.input.keyboard.addKey(
+      Phase.Keyboard.SPACEBAR
+    );
+    spaceKey.onDown.add(this.jump, this);
   },
 
   update: function() {
+    // if mothman is off screen restart
+    if (this.mothman.y < 0 || this.mothman.y > 490) {
+      this.restart();
+    }
+  },
 
+  jump: function() {
+    // Add vertical velocity
+    this.mothman.body.velocity.y = -350;
+  },
+
+  restart: function() {
+    //Restarts the game
+    game.state.start('main');
   }
 };
 
